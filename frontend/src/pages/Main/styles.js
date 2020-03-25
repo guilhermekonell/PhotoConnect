386f6062853b614a3bd00027c8bc8055287fba0f
guilhermekonell <gui.ri.konell@hotmail.com>
2020-03-24 21:08:51 -0300

    Init commit

---
+++ frontend/src/pages/Main/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 100px;
+
+  h1 {
+    font-size: 24px;
+    color: #7159c1;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
