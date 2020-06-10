32cb80aa0ac0367193dd0873f33ad1b78264c1f9
guilhermekonell <gui.ri.konell@hotmail.com>
2020-06-10 19:41:48 -0300

    Configure .env

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'photoconnect',
-  database: 'photoconnect',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
